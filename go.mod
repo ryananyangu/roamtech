78543ba7c9ad2cd6d81f73782d1ba9612a7125f2
Ryan Ashiruma <ryananyangu@gmail.com>
2022-04-20 15:12:32 +0300

    complete code

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/ryananyangu/roamtech
 
 go 1.16
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/gofiber/fiber/v2 v2.32.0 // indirect
+	golang.org/x/net v0.0.0-20220418201149-a630d4f3e7a2
+)
--- main.go
+++ main.go
@@ -1,5 +1,63 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
+)
 
+// HeadersMiddleware headers setup and defination
+func HeadersMiddleware(c *fiber.Ctx) error {
+
+	// crossDomain := string(c.Request().Header.Host())
+
+	// c.Set("Access-Control-Allow-Origin", crossDomain)
+	c.Set("Access-Control-Allow-Credentials", "true")
+	c.Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+	c.Set("Content-Type", "application/json")
+	return c.Next()
+
+}
+
+// SetupRouter specifiy routes
+func SetupRouter() *fiber.App {
+	app := fiber.New()
+	app.Use(HeadersMiddleware)
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+	}))
+	app.Use(logger.New(logger.Config{
+		Format:     "[${time}] [${ip}:${port}] ${pid} ${locals:requestid} ${status} ${latency} - ${method} ${path} ${query:}\n",
+		TimeFormat: "02-Jan-2006 15:04:05",
+		TimeZone:   "UTC",
+	}))
+
+	app.Get("/dashboard", monitor.New())
+
+	for path, handlers := range Routes {
+		for method, handler := range handlers {
+			switch method {
+			case "GET":
+				app.Get(path, handler)
+			case "POST":
+				app.Post(path, handler)
+			case "PUT":
+				app.Put(path, handler)
+			case "PATCH":
+				app.Patch(path, handler)
+			case "DELETE":
+				app.Delete(path, handler)
+			}
+		}
+	}
+	return app
+}
+
+func main() {
+	app := SetupRouter()
+
+	app.Listen(":8080")
 }
